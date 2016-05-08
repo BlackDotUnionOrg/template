2dff296866a42b283df9dd86a814a99f3a0cec1f
Benaka Moorthi <benaka.moorthi@mheducation.com>
2016-05-07 19:02:30 -0700

    Open about-us in new window.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,6 @@
 // JS for the homepage.
 $(document).ready(function () {
     $('.meet-the-founders').on('click', ':not(.sqs-block-code):not(.sqs-block-spacer)', function () {
-        window.location = '/about-us/'; // TODO: add a test for this?
+        window.open('/about-us/', "_blank"); // TODO: add a test for this?
     });
 });
