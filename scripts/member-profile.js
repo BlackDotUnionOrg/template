23c1f768bf2cf0c43cbcdba28b1d75ccaa8ad5df
Benaka Moorthi <benaka.moorthi@mheducation.com>
2016-05-21 20:28:26 -0700

    Make sure login/signup link is on upper right when not on homepage.

--- scripts/member-profile.js
+++ scripts/member-profile.js
@@ -1,9 +1,19 @@
+// change the login/signup link to say 'Hello, user' if the user is logged in
 $(document).on('gopaywall:loaded', function () {
     var firstName = window.userobj.user_data.username.replace(/^\s+|\s+$/g, '').split(/\s+/)[0];
     $('.login-profile-link')
-        .html($('<span>').text('Hello ' + firstName))
-        .detach()
-        .appendTo($('#siteWrapper'));
+        .html($('<span>').text('Hello ' + firstName));
 
     $('body').addClass('logged-in');
+});
+
+// if we're not on the homepage, move the login/signup link to the upper right
+$(document).ready(function () {
+    if ($('body.homepage').length) {
+        return;
+    }
+
+    $('.login-profile-link')
+        .detach()
+        .appendTo($('#siteWrapper'));
 });
